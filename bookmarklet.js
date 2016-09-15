045f476cf1881f0a6143201308f862ca4597e435
Ryan Baxley <rynobax@users.noreply.github.com>
2016-09-15 12:54:25 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,5 +6,30 @@ javascript:(function(){
         window.ryanDetectPropertyForOnlyOneInstance_dsljl23sdgsfdhsfdd326ag = true;
         
         alert("bookmarklet loaded");
+        
+        
+        createRow = (function() {
+        	var cached_function = createRow;
+        	
+        	return function() {
+	        	/*console.log("before");
+	        	console.debug(arguments);*/
+	        	var result = cached_function.apply(this, arguments);
+	        	return result;
+	    	};
+        })();
+        
+        onGetItemsResult = (function() {
+        	var cached_function = onGetItemsResult;
+        	
+        	return function() {
+	        	arguments[0].sort(function(a,b){
+				  return new Date(a.date) - new Date(b.date);
+				});
+	        	
+	        	var result = cached_function.apply(this, arguments);
+	        	return result;
+	    	};
+        })();
     }
-}
+}) ();
