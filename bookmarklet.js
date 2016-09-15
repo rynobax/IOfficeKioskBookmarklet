cb9f1c0dc89316fe0d76a497699b49290f12a42f
Ryan Baxley <rynobax@users.noreply.github.com>
2016-09-15 12:21:10 -0400

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:(function(){
+	/* Ryan Baxley 2016 */
+	if (window.hasOwnProperty('ryanDetectPropertyForOnlyOneInstance_dsljl23sdgsfdhsfdd326ag')) {
+		alert('"No Location" has already been loaded.');
+    }else{
+        window.ryanDetectPropertyForOnlyOneInstance_dsljl23sdgsfdhsfdd326ag = true;
+        
+        alert("bookmarklet loaded");
+    }
+}
